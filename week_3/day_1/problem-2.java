7d7c971954595376f8074cdee59e7dafbe01947c
Deepesh-Kumar-Singh <deepeshsing53@gmail.com>
2026-06-15 13:18:58 +0530

    Create problem-2.java

---
+++ week_3/day_1/problem-2.java
@@ -0,0 +1,34 @@
+// Reverse nodes in the kth group
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head==null) return null;
+        ListNode tail=head;
+        for(int i=0;i<k;i++){
+            if(tail==null) return head;
+            tail=tail.next;
+        }
+        ListNode ans=reverse(head,tail);
+        head.next=reverseKGroup(tail,k);
+        return ans;
+    }
+    public ListNode reverse(ListNode curr,ListNode end){
+    ListNode pre=null;
+    while(curr!=end){
+        ListNode next=curr.next;
+        curr.next=pre;
+        pre=curr;
+        curr=next;
+    }
+    return pre;
+    }
+}
