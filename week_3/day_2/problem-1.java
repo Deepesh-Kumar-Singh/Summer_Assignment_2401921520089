1f9d07eebcbaf77f042c6d55f58a3f80c46bd713
Deepesh-Kumar-Singh <deepeshsing53@gmail.com>
2026-06-16 18:43:04 +0530

    Create problem-1.java

---
+++ week_3/day_2/problem-1.java
@@ -0,0 +1,27 @@
+//Details-Process String with Special Operations I
+class Solution {
+    public String processStr(String s) {
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch >= 'a' && ch <= 'z'){
+                ans.append(ch);
+            }
+            else if(ch=='*'){
+               if (ans.length() > 0) {
+                    ans.deleteCharAt(ans.length() - 1);
+                }
+            }
+            else if(ch=='#'){
+                int len=ans.length();
+                for(int j=0;j<len;j++){
+                    ans.append(ans.charAt(j));
+                }
+            }
+            else{
+                ans.reverse();
+            }
+        }
+        return ans.toString();
+    }
+}
